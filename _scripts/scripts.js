10ddecd0f8b1a289adbdfb9e9703c58d7613f8c8
Eli <ely.nathan93@gmail.com>
2019-02-15 15:20:13 +0000

    Initial commit

---
+++ _scripts/scripts.js
@@ -0,0 +1,8 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Deals from './components/Deals'
+
+let dataBoxes = document.getElementById("data-boxes")
+
+ReactDOM.render( <Deals />, dataBoxes )
